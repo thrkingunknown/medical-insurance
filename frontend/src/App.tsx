30646e48f27edc688ae5a27d5b45e4f2fb83e69c
thrkingunknown <thekingunknown4@gmail.com>
2025-10-10 20:30:00 +0530

    feat: add @vercel/analytics to track application usage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { InsuranceForm } from "./components/InsuranceForm";
 import { ThemeProvider } from "./components/ThemeProvider";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="medical-insurance-theme">
+      <Analytics />
       <InsuranceForm />
     </ThemeProvider>
   );
